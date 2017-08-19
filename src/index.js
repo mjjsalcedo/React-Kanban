ba596d364e00ab368d11864a6a9024a24402f9c8
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-08-18 18:55:04 -1000

    routing

--- src/index.js
+++ src/index.js
@@ -8,15 +8,28 @@ import { Provider } from 'react-redux';
 import cardReducers from './containers/App/reducers';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 
 const store = createStore(
   cardReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(ReduxThunk)
 );
-render(
+
+ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <div>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </div>
+
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
   </Provider>,
-  document.getElementById('root')
-)
+  rootEl
+);
